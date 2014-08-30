92196be0b25adc4ea3c80eac31a9277ba0d94d32
Lucas Schneider <lschneider4@wisc.edu>
2014-08-30 11:29:24 -0500

    Update forceDigest.js

--- data/patronEditMods/forceDigest.js
+++ data/patronEditMods/forceDigest.js
@@ -1,8 +1,20 @@
 /*
- * notification: HTML checkbox input element which sends
+ * email: HTML checkbox input element which sends
  *   patron email notification
- * digestBox:    nottification's checkbox input option to
+ * digest:    nottification's checkbox input option to
  *   send digests only
  */
-function forceDigest(notification, digestBox) {
+function forceDigest(email, digest) {
+  if (email !== null && digest !== null) {
+    if (email.checked) digest.checked = true;
+    else digest.checked = false;
+  }
 }
+
+var email1 = document.getElementById('email1');
+var digest1 = document.getElementById('digest1');
+var email2 = document.getElementById('email1');
+var digest2 = document.getElementById('digest1');
+
+if (email1 !== null && digest1 !== null) email1.addEventListener('click', function() {forceDigest(email1,digest1)});
+if (email2 !== null && digest2 !== null) email2.addEventListener('click', function() {forceDigest(email2,digest2)});
