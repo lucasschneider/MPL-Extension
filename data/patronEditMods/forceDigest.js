31a61abacf83a5360f8015b141bf31298f23893e
Lucas Schneider <lschneider4@wisc.edu>
2014-08-30 11:01:27 -0500

    Init script to force digests only

---
+++ data/patronEditMods/forceDigest.js
@@ -0,0 +1,8 @@
+/*
+ * notification: HTML checkbox input element which sends
+ *   patron email notification
+ * digestBox:    nottification's checkbox input option to
+ *   send digests only
+ */
+function forceDigest(notification, digestBox) {
+}
