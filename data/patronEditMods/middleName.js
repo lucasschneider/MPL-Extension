a807414cffdd1d837af955697b668a5d8c690f96
Lucas Schneider <lschneider4@wisc.edu>
2014-08-30 16:00:22 -0500

    Update middleName.js

--- data/patronEditMods/middleName.js
+++ data/patronEditMods/middleName.js
@@ -1,9 +1,17 @@
 function parseName() {
   var firstName = document.getElementById('firstname');
-  if (firstName !== null) {
-    var names = firstName.split(' ');
-    names.splice(0,1);
-    for (var n in names) {
+  var initials = document.getElementById('initials');
+  if (firstName !== null && initials !== null) {
+    var names = firstName.value.split(' ');
+    var len = names.length;
+    if (len > 1) initials.value = names[1][0];
+    else initials.value = " ";
+    if (len > 2) {
+      initials.value += " "
+      for (var i = 2; i < len; i++) {
+        if (i === len - 1) initials.value += names[i][0];
+        else initials.value += names[i][0] + " ";
+      }
     }
   }
 }
