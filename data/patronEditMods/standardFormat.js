b36e6fda5c8b7a298adbe9e24af05c6156d8886c
Lucas Schneider <lschneider4@wisc.edu>
2014-08-30 21:48:27 -0500

    Standardize patron record formatting

--- data/patronEditMods/standardFormat.js
+++ data/patronEditMods/standardFormat.js
@@ -1,7 +1,20 @@
 /*** CORRECT TEXT CASE ***/
-for (var input in document.getElementsByTagName('input')) {
-  if (input.type === 'text') {
-    if (/email|emailpro|B_email/.test(input.id)) input.addEventListener('blur', function() {this.value.toLowerCase()});
-    else input.addEventListener('blur', function() {this.value.toUpperCase()});
+var inputs = document.querySelectorAll("input[type=text]");
+if (inputs !== null) {
+  for (var i = 3; i < inputs.length; i++) {
+    if (inputs[i].type === 'text') {
+      if (/email|emailpro|B_email/.test(inputs[i].id)) inputs[i].addEventListener('blur', function() {this.value = this.value.toLowerCase();});
+      else inputs[i].addEventListener('blur', function() {this.value = this.value.toUpperCase();});
+    }
   }
 }
+
+/*** CORRECT CITY FORMAT ***/
+var city = document.getElementById('city');
+if (city !== null) city.addEventListener('blur', function() {
+  if (/madison(,? wi(sconsin)?)?|mad/i.test(this.value)) this.value = "MADISON, WI";
+});
+
+/*** ALWAYS CHECK HOLD NOTIFICATION ***/
+var hold = document.getElementById('email4');
+if (hold !== null && hold.checked == false) hold.checked = true;
