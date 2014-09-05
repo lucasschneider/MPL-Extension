7ca292a9ba4827df816bad405470bd1825669c74
Lucas Schneider <lschneider4@wisc.edu>
2014-09-04 23:29:28 -0500

    Progress toward census tract lookup

--- data/patronEditMods/collegeExp.js
+++ data/patronEditMods/collegeExp.js
@@ -5,7 +5,7 @@ function fillDormExp() {
   var addr2 = document.getElementById('address2');
   var zip = document.getElementById('zipcode');
   
-  if (zip !== null && addr !== null) {
+  if (zip !== null && addr !== null && zip.value === '') {
     addrRegEx = /[ ]*15(10|20) tripp.*|[ ]*970 university.*|[ ]*(625|635|640|650) elm.*|[ ]*(35|420).{0,7}park.*|[ ]*1200 observatory.*|[ ]*16(35|50) kronshage.*|[ ]*(835|917|919|921).{0,6}dayton.*|[ ]*1950 willow.*|[ ]*(615|821|917).{0,6}johnson.*|[ ]*625 babcock.*/i;
     zipRegEx = /53706(\-[0-9]{4})?|53715(\-[0-9]{4})?/;
     var addressVal = addr2 !== null ? addr.value + " " + addr2.value : addr.value;
