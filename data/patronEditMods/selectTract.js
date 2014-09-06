4188cb02a17fd8e5eccb19a178f43fdfb9c911f4
Lucas Schneider <lschneider4@wisc.edu>
2014-09-06 11:43:43 -0500

    deleted:    data/patronEditMods/preselectMADPstat.js 	modified:   data/patronEditMods/selectTract.js 	modified:   lib/main.js 	modified:   package.json

--- data/patronEditMods/selectTract.js
+++ data/patronEditMods/selectTract.js
@@ -52,7 +52,6 @@ if (addr !== null) {
 }
 city = document.getElementById('city');
 if (city !== null) city.addEventListener('blur', queryCensusTract);
-
 // Select D-X-MAD form PSTAT select list
 function selectXMAD(selectList) {
   if (selectList !== null) {
