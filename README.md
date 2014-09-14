99b525eff0568fb6d07c1b95b932b7d3a3216068
Lucas Schneider <lschneider4@wisc.edu>
2014-09-13 23:42:29 -0500

    Progress toward County Subdivisions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## TODO
 1. Separate 'County Subdivision' and address queries into two separate requests to increase reliability of results from the geocoder.
+-- return and autofill address info --> then query for town
+--- if town, change pstat to Town of Madison
+--- if not town, don't change
+--- if town is null, set to MAD-UND
--- data/patronEditMods/selectTract.js
+++ data/patronEditMods/selectTract.js
@@ -43,49 +43,29 @@ function fillDormExp() {
   }
 }
 
-// Select D-X-MAD form PSTAT select list
-function selectXMAD(selectList) {
-  if (selectList !== null) {
-    var madUndIdx = 235; /* D-X-MAD index as of 9/11/2014 */
-    if (selectList.children[madUndIdx].value === "D-X-MAD") {
-      selectList.selectedIndex = madUndIdx;
-    }
-    else {
-      for (var i = 0; i < selectList.length; i++) {
-        if (selectList.children[i].value === "D-X-MAD") {
-          selectList.selectedIndex = i;
-          break;
+function selectPSTAT(selectList, value) {
+  if (selectList !== null && value !== null) {
+    for (var i = 0; i < selectList.length; i++) {
+      if (selectList.children[i].value === value) {
+        selectList.selectedIndex = i;
+        var result = document.getElementById('tractResult');
+        if (result !== null) {
+          result.setAttribute('style','display:inline-block;color:#00c000;');
+          result.textContent = '[MATCH: '+matchAddr+']';
         }
+        break;
       }
-      selectList.selectedIndex = i;
     }
   }
 }
 
-function selectMADTown(selectList) {
-  if (selectList !== null) {
-    var madTownIdx = 303; /* D-MAD-T index as of 9/11/2014 */
-    if (selectList.children[madTownIdx].value === "D-MAD-T") {
-      selectList.selectedIndex = madTownIdx;
-    }
-    else {
-      for (var i = 0; i < selectList.length; i++) {
-        if (selectList.children[i].value === "D-MAD-T") {
-          selectList.selectedIndex = i;
-          break;
-        }
-      }
-      selectList.selectedIndex = i;
-    }
-  }
-}
-
-function trimAddr(addr) {
+function cleanAddr(addr) {
   addrParts = addr.split(" ");
   addrTrim = '';
-  if (addrParts[0] !== undefined) addrTrim += encodeURIComponent(addrParts[0]);
-  if (addrParts[1] !== undefined) addrTrim += "+" + encodeURIComponent(addrParts[1]);
-  if (addrParts[2] !== undefined) addrTrim += "+" + encodeURIComponent(addrParts[2]);
+  for (var i = 0; i < addrParts.length; i++) {
+    if (i === 0) addrTrim += encodeURIComponent(addrParts[i]);
+    else addrTrim += " " + encodeURIComponent(addrParts[i]);
+  }
   return addrTrim;
 }
 
@@ -97,62 +77,50 @@ function queryCensusTract() {
   var notice = document.getElementById('tractNotice');
 
   if (addr.value !== "" && city.value !== "" && zip !== null && selectList !== null) {
-    var cityRegEx = /madison(,? wi(sconsin)?)?/i;
-    if (cityRegEx.test(document.getElementById('city').value)) {
-      // Generate loading message
-      notice.textContent = "Searching for census tract and zipcode... ";
-      var result = document.createElement('div');
-      result.setAttribute('id','tractResult');
-      notice.appendChild(result);
-      result.textContent = '';
-      setTimeout(function() {
-      	var tr = document.getElementById('tractResult');
-      	if (tr !== null && tr.textContent === '') {
-	  tr.setAttribute('style','display:inline-block');
-	  tr.textContent = '[NOTE: Server slow to respond—please enter zipcode and census tract manually]';
-	}
-      }, 6000);
+    // Generate loading message
+    notice.textContent = "Searching for census tract and zipcode... ";
+    var result = document.createElement('div');
+    result.setAttribute('id','tractResult');
+    notice.appendChild(result);
+    result.textContent = '';
+    setTimeout(function() {
+      var tr = document.getElementById('tractResult');
+      if (tr !== null && tr.textContent === '') {
+        tr.setAttribute('style','display:inline-block');
+        tr.textContent = '[NOTE: Server slow to respond—please enter zipcode and census tract manually]';
+      }
+    }, 6000);
 
-      // Default to MAD UND if tract is empty
-      if (selectList[selectList.selectedIndex] === "") selectXMAD(selectList);
-      self.port.emit("queryTract", trimAddr(addr.value));
-      self.port.on("receivedTract", function (addrTract) {
-        var results = false;
-        if (addrTract !== null && addrTract.length === 4) {
-	  var matchAddr = addrTract[0]
-	  zip.value = addrTract[1];
-	  fillDormExp();
-
-          if (addrTract[3] !== null && addrTract[3]) {
-            selectMADTown(selectList);
-            result.setAttribute('style','display:inline-block;color:#00c000;');
-            result.textContent = '[MATCH: '+matchAddr+']';
-            results = true;
-	  }
-          else if (addrTract[3] !== null) {
-            for (var i = 0; i < selectList.length; i++) {
-              if (selectList.children[i].value === "D-"+addrTract[2]) {
-                selectList.selectedIndex = i;
-                result.setAttribute('style','display:inline-block;color:#00c000;');
-                result.textContent = '[MATCH: '+matchAddr+']';
-                results = true;
-                break;
-              }
-            }
-	  }
-	  else {
-            selectXMAD(selectList);
+    self.port.emit("queryCntySub", cleanAddr(addr.value));
+    self.port.on("receivedCntySub", function (cntySub) {
+      self.port.emit("queryTract", [cntySub,cleanAddr(addr.value)]);
+    });
+    self.port.on("receivedTract", function (cntySubAddrTract) {
+      var cntySub = cntySubAddrTract[0];
+      var addrTract = cntySubAddrTract[1];
+      var results = false;
+      if (addrTract !== null && addrTract.length === 3) {
+        var matchAddr = addrTract[0];
+        zip.value = addrTract[1];
+        fillDormExp();
+     
+        switch (cntySub) {
+	  case "Blooming Grove town":
+	    selectPSTAT(selectList, "D-BG-T");
+	    break;
+	  case "Madison city":
+	    selectPSTAT(selectList, "D-"+addrTract[2]);
+	    break;
+	  case "Madison town":
+	    selectPSTAT(selectList, "D-MAD-T");
+	    break;
+	  default:
+	    selectUND(selectList, "X-UND");
             result.setAttribute('style','display:inline-block');
-            result.textContent = "[FAILED: unable to determine whether addresss was Town of Madison;\nplease enter zipcode and census tract manually.]";
-            results = true;
-          }
-	}
-        if (!results) {
-	  selectXMAD(selectList);
-	  result.setAttribute('style','display:inline-block');
-	  result.textContent = '[FAILED: please enter zipcode and census tract manually.]';
-	}
-      });
-    }
+            result.textContent = "[FAILED: unable to determine county subdivision; please enter zipcode and PSTAT manually.]";
+	    break;
+        }
+      }
+    });
   }
 }
