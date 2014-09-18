d2eb1f3e3f94e016d86d2e14387609e785a907f3
Lucas Schneider <lschneider4@wisc.edu>
2014-09-18 17:40:22 -0500

    Only use initial of first middle name; no trailing spaces

--- data/patronEditMods/middleName.js
+++ data/patronEditMods/middleName.js
@@ -2,17 +2,10 @@ function parseName() {
   var firstName = document.getElementById('firstname');
   var initials = document.getElementById('initials');
   if (firstName !== null && initials !== null) {
-    var names = firstName.value.split(' ');
+    var names = trim(firstName.value).split(' ');
     var len = names.length;
     if (len > 1) initials.value = names[1][0];
-    else initials.value = " ";
-    if (len > 2) {
-      initials.value += " "
-      for (var i = 2; i < len; i++) {
-        if (i === len - 1) initials.value += names[i][0];
-        else initials.value += names[i][0] + " ";
-      }
-    }
+    else initials.value = "";
   }
 }
 
