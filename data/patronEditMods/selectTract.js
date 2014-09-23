302e062dfa43bbd0044e2ba0eb1229b41ea48fad
unknown <madcen@MADSD46.scls.local>
2014-09-22 20:48:13 -0500

    PSTAT progress

--- data/patronEditMods/selectTract.js
+++ data/patronEditMods/selectTract.js
@@ -48,9 +48,11 @@ function fillDormExp() {
 
 function selectPSTAT(selectList, value, result, matchAddr) {
   if (selectList !== null && value !== null && result !== null && matchAddr !== null) {
+    var selected = false;
     for (var i = 0; i < selectList.length; i++) {
       if (selectList.children[i].value === value) {
         selectList.selectedIndex = i;
+        selected = true;
         if (result !== null) {
           result.setAttribute('style','display:inline-block;color:#00c000;');
           result.textContent = '[MATCH: '+matchAddr+']';
@@ -58,6 +60,21 @@ function selectPSTAT(selectList, value, result, matchAddr) {
         break;
       }
     }
+    if (!selected) selectUND(selectList);
+  }
+}
+
+function selectMID(selectList, result, matchAddr) {
+  if (selectList !== null && result !== null && matchAddr !== null) {
+    if (/.*a(spen|urora).*|.*b(lackhawk|lackwood).*|(18|19)[0-9]{2} bristol.*/i.test(matchAddr)) selectPSTAT(selectList, "D-MID-C1", result, matchAddr);
+    else if (/.*b(oundary|riarcliff).*/i.test(matchAddr)) selectPSTAT(selectList, "D-MID-C2", result, matchAddr);
+    else if (/.*anderson.*|.*b(eechwood|oulder).*|^[0-9]*[02468]$ branch.*|2[0-9]{3} bristol.*|.*mayflower.*/i.test(matchAddr)) selectPSTAT(selectList, "D-MID-C3", result, matchAddr);
+    else if (/.*a(dler|llen|mherst).*|^[0-9]*[13579]$ branch.*/i.test(matchAddr)) selectPSTAT(selectList, "D-MID-C4", result, matchAddr);
+    else if (/.*airport.*|.*alpha.|.*b(auer|elle fontaine|lack opal|ravo).*/i.test(matchAddr)) selectPSTAT(selectList, "D-MID-C5", result, matchAddr);
+    else if (/.*a(l(do leopold|gonquin)|pprentice|ssociates).*|.*black cherry.*/i.test(matchAddr)) selectPSTAT(selectList, "D-MID-C6", result, matchAddr);
+    else if (/.*a(nna|ster).*|.*baskerville.*/i.test(matchAddr)) selectPSTAT(selectList, "D-MID-C7", result, matchAddr);
+    else if (/.*brindisi.*/i.test(matchAddr)) selectPSTAT(selectList, "D-MID-C8", result, matchAddr);
+    else selectUND(selectList);
   }
 }
 
@@ -77,7 +94,7 @@ function cleanAddr(addr) {;
   addrTrim = '';
   for (var i = 0; i < addrParts.length; i++) {
     if (i === 0) addrTrim += encodeURIComponent(addrParts[i]);
-    else addrTrim += " " + encodeURIComponent(addrParts[i]);
+    else addrTrim += "+" + encodeURIComponent(addrParts[i]);
   }
   return addrTrim;
 }
@@ -109,8 +126,6 @@ function queryCensusTract() {
       self.port.emit("queryTract", cleanAddr(addr));
     });
     self.port.on("receivedTract", function (addrTract) {
-      var addrTract = addrTract;
-      var results = false;
       if (addrTract !== null && addrTract.length === 3) {
         var matchAddr = addrTract[0];
         zip.value = addrTract[1];
@@ -125,6 +140,12 @@ function queryCensusTract() {
           case "Madison town":
             selectPSTAT(selectList, "D-MAD-T", result, matchAddr);
             break;
+          /*case "Middleton city":
+            selectMID(selectList, result, matchAddr);
+            break;
+          case "Middleton town":
+            selectPSTAT(selectList, "D-MID-T", result, matchAddr);
+            break;*/
           default:
             selectUND(selectList);
             result.setAttribute('style','display:inline-block');
