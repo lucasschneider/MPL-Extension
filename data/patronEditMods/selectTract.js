e191eae9803c544b518b366fcf83b760518936df
Lucas Schneider <lschneider4@wisc.edu>
2014-09-25 20:46:58 -0500

    PSTAT lookup progress

--- data/patronEditMods/selectTract.js
+++ data/patronEditMods/selectTract.js
@@ -42,46 +42,10 @@
     return cty;
   }
 
-// COPIED FROM collegeExp.js
-  function fillDormExp() {
-    var addr = document.getElementById('address'),
-      addr2 = document.getElementById('address2'),
-      zip = document.getElementById('zipcode'),
-      expiry = document.getElementById('dateexpiry'),
-      addrRegEx = /[ ]*15(10|20) tripp.*|[ ]*970 university.*|[ ]*(625|635|640|650) elm.*|[ ]*(35|420).{0,7}park.*|[ ]*1200 observatory.*|[ ]*16(35|50) kronshage.*|[ ]*(835|917|919|921).{0,6}dayton.*|[ ]*1950 willow.*|[ ]*(615|821|917).{0,6}johnson.*|[ ]*625 babcock.*/i,
-      zipRegEx = /53706(\-[0-9]{4})?|53715(\-[0-9]{4})?/,
-      addressVal,
-      date,
-      year;
-
-    if (zip !== null && addr !== null && expiry.value === '') {
-      addressVal = addr2 !== null ? addr.value + " " + addr2.value : addr.value;
-      if (zipRegEx.test(zip.value) && addrRegEx.test(addressVal)) {
-        date = new Date();
-        switch (parseInt(date.getUTCMonth(), 10)) {
-        case 0:
-        case 1:
-        case 2:
-        case 3:
-          year = date.getUTCFullYear();
-          break;
-        case 4:
-          if (parseInt(date.getUTCDate(), 10) < 15) {
-            year = date.getUTCFullYear();
-          }
-          break;
-        default:
-          year = (parseInt(date.getUTCFullYear(), 10) + 1).toString();
-          break;
-        }
-        expiry.value = "05/15/" + year;
-      }
-    }
-  }
-
   function selectUND(selectList) {
-    var i;
-    if (selectList !== null) {
+    var addr = document.getElementById('address'),
+      i;
+    if (addr !== null && addr.value === '' && selectList !== null) {
       for (i = 0; i < selectList.length; i++) {
         if (selectList.children[i].value === "X-UND") {
           selectList.selectedIndex = i;
@@ -99,7 +63,7 @@
         if (selectList.children[i].value === value) {
           selectList.selectedIndex = i;
           selected = true;
-          if (result !== null) {
+          if (result !== null && value !== "D-X-SUN") {
             result.setAttribute('style', 'display:inline-block;color:#00c000;');
             result.textContent = '[MATCH: ' + matchAddr + ']';
           }
@@ -107,30 +71,6 @@
         }
       }
       if (!selected) {
-        selectUND(selectList);
-      }
-    }
-  }
-
-  function selectMID(selectList, result, matchAddr) {
-    if (selectList !== null && result !== null && matchAddr !== null) {
-      if (/.*a(spen|urora).*|.*b(lackhawk|lackwood).*|(18|19)[0-9]{2} bristol.*/i.test(matchAddr)) {
-        selectPSTAT(selectList, "D-MID-C1", result, matchAddr);
-      } else if (/.*b(oundary|riarcliff).*/i.test(matchAddr)) {
-        selectPSTAT(selectList, "D-MID-C2", result, matchAddr);
-      } else if (/.*anderson.*|.*b(eechwood|oulder).*|^[0-9]*[02468]$ branch.*|2[0-9]{3} bristol.*|.*mayflower.*/i.test(matchAddr)) {
-        selectPSTAT(selectList, "D-MID-C3", result, matchAddr);
-      } else if (/.*a(dler|llen|mherst).*|^[0-9]*[13579]$ branch.*/i.test(matchAddr)) {
-        selectPSTAT(selectList, "D-MID-C4", result, matchAddr);
-      } else if (/.*airport.*|.*alpha.|.*b(auer|elle fontaine|lack opal|ravo).*/i.test(matchAddr)) {
-        selectPSTAT(selectList, "D-MID-C5", result, matchAddr);
-      } else if (/.*a(l(do leopold|gonquin)|pprentice|ssociates).*|.*black cherry.*/i.test(matchAddr)) {
-        selectPSTAT(selectList, "D-MID-C6", result, matchAddr);
-      } else if (/.*a(nna|ster).*|.*baskerville.*/i.test(matchAddr)) {
-        selectPSTAT(selectList, "D-MID-C7", result, matchAddr);
-      } else if (/.*brindisi.*/i.test(matchAddr)) {
-        selectPSTAT(selectList, "D-MID-C8", result, matchAddr);
-      } else {
         selectUND(selectList);
       }
     }
@@ -155,7 +95,7 @@
         }
       }, 7000);
 
-      console.log(cleanAddr(addr) + " :: " + pullCity(city.value));
+      console.log("clean addr: " + cleanAddr(addr) + " pulled city: " + pullCity(city.value));
       self.port.emit("queryCntySub", [cleanAddr(addr), pullCity(city.value)]);
       self.port.on("receivedCntySub", function (cntySub) {
         console.log('received cnty sub: ' + cntySub);
@@ -169,23 +109,181 @@
         if (addrTract !== null && addrTract.length === 3) {
           var matchAddr = addrTract[0];
           zip.value = addrTract[1];
-          fillDormExp();
+          // Switch based on county subdivision
           switch (window.cntySub) {
+
+          /*** FREQUENTLY USED PSTATS ***/
           case "Blooming Grove town":
             selectPSTAT(selectList, "D-BG-T", result, matchAddr);
             break;
+          case "Fitchburg city":
+            selectPSTAT(selectList, "D-FIT-T", result, matchAddr);
+            break;
           case "Madison city":
             selectPSTAT(selectList, "D-" + addrTract[2], result, matchAddr);
+            // Defined in collegeExp.js
+            window.fillDormExp();
             break;
           case "Madison town":
             selectPSTAT(selectList, "D-MAD-T", result, matchAddr);
             break;
-          case "Middleton city":
-            selectMID(selectList, result, matchAddr);
-            break;
           case "Middleton town":
             selectPSTAT(selectList, "D-MID-T", result, matchAddr);
             break;
+
+          /*** UNDETERMINABLE COUNTY SUBDIVISIONS ***/
+          case "Sun Prairie city":
+            selectPSTAT(selectList, "D-X-SUN", result, matchAddr);
+            result.setAttribute('style', 'display:inline-block');
+            result.textContent = "[FAILED: automatic generation only occurs for libraries that sort by census tract or county subdivision.]";
+            break;
+          case "Middleton city":
+          case "Monona city":
+          case "Verona city":
+            selectUND(selectList);
+            result.setAttribute('style', 'display:inline-block');
+            result.textContent = "[FAILED: automatic generation only occurs for libraries that sort by census tract or county subdivision.]";
+            break;
+
+          /*** MAIN LIST ***/
+          case "Adams city":
+            selectPSTAT(selectList, "A-ADM-C", result, matchAddr);
+            break;
+          // For "Adams town" (Adams County) see DUPLICATE SUBDIVISION NAMES
+          // For "Adams town" (Green County) see DUPLICATE SUBDIVISION NAMES
+          case "Alban town":
+            selectPSTAT(selectList, "P-ALB-T", result, matchAddr);
+            break;
+          case "Albany town":
+            selectPSTAT(selectList, "G-ALB-T2", result, matchAddr);
+            break;
+          case "Albany village":
+            selectPSTAT(selectList, "G-ALB-V", result, matchAddr);
+            break;
+          case "Albion town":
+            selectPSTAT(selectList, "D-ALB-T", result, matchAddr);
+            break;
+          case "Almond town":
+            selectPSTAT(selectList, "P-ALM-T", result, matchAddr);
+            break;
+          case "Almond village":
+            selectPSTAT(selectList, "P-ALM-V", result, matchAddr);
+            break;
+          case "Amherst Junction village":
+            selectPSTAT(selectList, "P-AMJ-V", result, matchAddr);
+            break;
+          case "Amherst town":
+            selectPSTAT(selectList, "P-AMH-T", result, matchAddr);
+            break;
+          case "Amherst village":
+            selectPSTAT(selectList, "P-AMH-V", result, matchAddr);
+            break;
+          // For "Arena town" see RECIPROCAL COUNTIES
+          // For "Arena village" see RECIPROCAL COUNTIES
+          case "Arlington town":
+            selectPSTAT(selectList, "C-ARL-T", result, matchAddr);
+            break;
+          case "Arlington village":
+            selectPSTAT(selectList, "C-ARL-V", result, matchAddr);
+            break;
+          case "Arpin town":
+            selectPSTAT(selectList, "W-ARP-T", result, matchAddr);
+            break;
+          case "Arpin village":
+            selectPSTAT(selectList, "W-ARP-V", result, matchAddr);
+            break;
+          // For "Ashippun town" see RECIPROCAL COUNTIES
+          case "Auburndale town":
+            selectPSTAT(selectList, "W-AUB-T", result, matchAddr);
+            break;
+          case "Auburndale village":
+            selectPSTAT(selectList, "W-AUB-V", result, matchAddr);
+            break;
+          // For "Avoca village" see RECIPROCAL COUNTIES
+          // For "Aztalan town" see RECIPROCAL COUNTIES
+          case "Baraboo city":
+            selectPSTAT(selectList, "S-BAR-C1", result, matchAddr);
+            break;
+          case "Baraboo town":
+            selectPSTAT(selectList, "S-BAR-T", result, matchAddr);
+            break;
+          // For "Barneveld village" see RECIPROCAL COUNTIES
+          case "Bear Creek town":
+            selectPSTAT(selectList, "S-BC-T", result, matchAddr);
+            break;
+          // For "Beaver Dam city" see RECIPROCAL COUNTIES
+          // For "Beaver Dam town" see RECIPROCAL COUNTIES
+          // For "Belleville village" (DANE COUNTY) see DUPLICATE SUBDIVISION NAMES
+          // For "Belleville village" (GREEN COUNTY) see DUPLICATE SUBDIVISION NAMES
+          case "Belmont town":
+            selectPSTAT(selectList, "P-BEL-T", result, matchAddr);
+            break;
+          // For "Beloit city" see RECIPROCAL COUNTIES
+          // For "Berlin city" see RECIPROCAL COUNTIES
+          case "Berry town":
+            selectPSTAT(selectList, "D-BERR-T", result, matchAddr);
+            break;
+          case "Big Flats town":
+            selectPSTAT(selectList, "D-BIG-T", result, matchAddr);
+            break;
+          case "Biron village":
+            selectPSTAT(selectList, "W-BIR-V", result, matchAddr);
+            break;
+          case "Black Earth town":
+            selectPSTAT(selectList, "D-BE-T", result, matchAddr);
+            break;
+          case "Black Earth village":
+            selectPSTAT(selectList, "B-BE-V", result, matchAddr);
+            break;
+          // For "Blanchardville village" see RECIPROCAL COUNTIES
+          // For "Blooming Grove town" see FREQUENTLY USED PSTATS
+          case "":
+            selectPSTAT(selectList, "", result, matchAddr);
+            break;
+          // For "Fitchburg city" see FREQUENTLY USED PSTATS
+          // For "Madison city" see FREQUENTLY USED PSTATS
+          // For "Madison town" see FREQUENTLY USED PSTATS
+          // For "Middleton city" see UNDETERMINABLE COUNTY SUBDIVISIONS
+          // For "Middleton town" see FREQUENTLY USED PSTATS
+          // For "Monona city" see UNDETERMINABLE COUNTY SUBDIVISIONS
+          // For "Sun Prairie city" see UNDETERMINABLE COUNTY SUBDIVISIONS
+          // For "Verona city" see UNDETERMINABLE COUNTY SUBDIVISIONS
+
+          /*** RECIPROCAL COUNTIES ***/
+          case "Arena town": // IOWA COUNTY
+            selectPSTAT(selectList, "O-SWLS-ART", result, matchAddr);
+            break;
+          case "Arena village": // IOWA COUNTY
+            selectPSTAT(selectList, "O-SWLS-ARV", result, matchAddr);
+            break;
+          case "Ashippun town": // DODGE COUNTY
+            selectPSTAT(selectList, "O-MWFLS", result, matchAddr);
+            break;
+          case "Avoca village": // IOWA COUNTY
+            selectPSTAT(selectList, "O-SWLS-AVV", result, matchAddr);
+            break;
+          case "Aztalan town": // JEFFERSON COUNTY
+            selectPSTAT(selectList, "O-MWFLS-AZT", result, matchAddr);
+            break;
+          case "Barneveld village": // IOWA COUNTY
+            selectPSTAT(selectList, "O-SWLS-BAV", result, matchAddr);
+            break;
+          case "Beaver Dam city": // DODGE COUNTY
+            selectPSTAT(selectList, "O-MWFLS-BVC", result, matchAddr);
+            break;
+          case "Beaver Dam town": // DODGE COUNTY
+            selectPSTAT(selectList, "O-MWFLS-BVT", result, matchAddr);
+            break;
+          case "Beloit city": // ROCK COUNTY
+            selectPSTAT(selectList, "O-ALS-BEL-C", result, matchAddr);
+            break;
+          case "Berlin city": // GREEN LAKE COUNTY
+            selectPSTAT(selectList, "O-WLS-BLC", result, matchAddr);
+            break;
+          case "Blanchardville village":
+            selectPSTAT(selectList, "O-SWLS-BLA", result, matchAddr);
+            break;
+          /*** DUPLICATE SUBDIVISION NAMES AND DEFAULT ***/
           default:
             selectUND(selectList);
             result.setAttribute('style', 'display:inline-block');
