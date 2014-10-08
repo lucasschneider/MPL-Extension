67bb432bd7ad31799b349ac3d494aae0f3de9631
Lucas Schneider <lschneider4@wisc.edu>
2014-10-08 16:58:23 -0500

    Package update

--- data/patronMessages.js
+++ data/patronMessages.js
@@ -3,7 +3,6 @@ var msgSelect = document.getElementById('type');
 
 if(msgSelect != null) {
   if (msgSelect.options[4].value === "Special Note") msgSelect.remove(4);
-  if (msgSelect.options[1].value === "Internet Authorization") msgSelect.remove(1);
 
   var cardAtNxtCko = document.createElement('option');
   cardAtNxtCko.value = "Patron must use library card at next checkout. ";
