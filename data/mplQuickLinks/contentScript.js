4a803d58642f658be40c8ec5ccb33358341769a3
Lucas Schneider <lschneider4@wisc.edu>
2015-02-09 20:14:15 -0600

    Update contentScript.js

--- data/mplQuickLinks/contentScript.js
+++ data/mplQuickLinks/contentScript.js
@@ -1,14 +1,14 @@
 var opacNote = document.getElementById('opacnote'),
   circNote = document.getElementById('borrowernotes'),
   categoryCode = document.getElementsByClassName('categorycode'),
- startingBalance,
- date,
- month,
- day,
- incrementYear = false,
- year,
- expiryDate = '',
- currDate = '';
+  startingBalance,
+  date,
+  month,
+  day,
+  incrementYear = false,
+  year,
+  expiryDate = '',
+  currDate = '';
 if (opacNote !== null && circNote !== null) {
   startingBalance = prompt('What is the patron\'s starting balance for this payment plan?');
   if (startingBalance !== null && startingBalance !== '') {
@@ -40,7 +40,6 @@ if (opacNote !== null && circNote !== null) {
     if (circNote.value !== null && circNote.value !== '') {
       circNote.value += "\n\n";
     }
-    opacNote.value += 'AT MADISON PUBLIC LIBRARY ONLY, you are allowed to checkout if you pay $1.00 per item. FULL payment is required outside of MPL. NO Outerloan or Rental checkouts allowed while on the plan. Holds are allowed only as copy specific on MPL items. Plan is void if new fees are added. Patron’s account is limited use while they are on the plan. Starting balance was $' + startingBalance + '. Charges must be paid by ' + expiryDate + '. Plan started on '+ currDate + ' ';
     circNote.value += 'AT MADISON PUBLIC LIBRARY ONLY, patron is allowed to checkout if they pay $1.00 per item. FULL payment is required outside of MPL. NO Outerloan or Rental checkouts allowed while on the plan. Holds are allowed only as copy specific on MPL items. Plan is void if new fees are added. Patron’s account is limited use while they are on the plan. Starting balance was $' + startingBalance + '. Charges must be paid by ' + expiryDate + '. Plan started on '+ currDate + ' ';
     if (categoryCode !== null && categoryCode[0].value === 'AD') {
       categoryCode[0].value = 'LU';
