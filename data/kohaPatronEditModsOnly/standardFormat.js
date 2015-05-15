4f885c51ebf99d45b545a740ae1e1402e9aff61c
Lucas Schneider <lschneider4@wisc.edu>
2015-05-15 01:07:48 -0500

    v6 Disable discourged patron fields

--- data/kohaPatronEditModsOnly/standardFormat.js
+++ data/kohaPatronEditModsOnly/standardFormat.js
@@ -39,8 +39,71 @@ function parseMadisonWI () {
   this.value = this.value.replace(/,/,'');
 }
 
-/*** ALWAYS CHECK HOLD NOTIFICATION ***/
-/*** This should be rewritten to only check the hold notification
- * option for new juvenile accounts */
-//var hold = document.getElementById('email4');
-//if (hold !== null && hold.checked == false) hold.checked = true;
+/*** DISABLE RARELY USED FIELDS ***/
+var unusedFields = ['streetnumber',
+  'address2',
+  'select_city',
+  'country',
+  'mobile',
+  'fax',
+  'B_country',
+  'B_address2',
+  'altcontactcountry',
+  'altcontactaddress2',
+  'sort2',
+  'email6', // Item checkout notification
+  'email7', // Hold cancelled notification
+  'email9', // Item lost notification
+  'email5', // Item check-in notification
+  // The following are the inputs for old dynix data
+  'patron_attr_1',
+  'patron_attr_2',
+  'patron_attr_3',
+  'patron_attr_4',
+  'patron_attr_5',
+  'patron_attr_6',
+  'patron_attr_7',
+  'patron_attr_8',
+  'patron_attr_9',
+  'patron_attr_10',
+  'patron_attr_11'],
+  parentElt = document.getElementById('entryform');
+  sibling = parentElt.children[0];
+  enableOptsLabel = document.createElement('label'),
+  enableOpts = document.createElement('input'),
+  enableOptsContainer = document.createElement('div'),
+  tmp = null;
+
+enableOptsLabel.setAttribute('for','enableOpts');
+enableOptsLabel.setAttribute('style','display: inline-block; font-weight: bold;');
+enableOptsLabel.textContent = 'Enable rarely used and discouraged input fields:';
+
+enableOpts.id = "enableOpts";
+enableOpts.type = 'checkbox';
+enableOpts.checked = 'true';
+enableOpts.setAttribute('style','margin-left: 20px; display: inline-block;');
+enableOpts.addEventListener('click', function () {
+  if (this.checked) {
+    for (i = 0; i < unusedFields.length; i++) {
+      elt = document.getElementById(unusedFields[i]);
+      if (elt !== null) {
+        tmp = elt.disabled = false;
+      }
+    }
+  } else {
+    for (i = 0; i < unusedFields.length; i++) {
+      elt = document.getElementById(unusedFields[i]);
+      if (elt !== null) {
+        tmp = elt.disabled = true;
+      }
+    }
+  }
+});
+
+enableOptsContainer.appendChild(enableOptsLabel);
+enableOptsContainer.appendChild(enableOpts);
+enableOptsContainer.style = "margin-left: 40px;";
+parentElt.insertBefore(enableOptsContainer, sibling);
+
+// Trigger event : disable fields
+enableOpts.click();
