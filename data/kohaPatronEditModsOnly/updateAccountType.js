16f8dbdb95992dbec4778de15096fb19cb61e545
Lucas Schneider <lschneider4@wisc.edu>
2015-07-01 21:02:43 -0500

    v8 progress

--- data/kohaPatronEditModsOnly/updateAccountType.js
+++ data/kohaPatronEditModsOnly/updateAccountType.js
@@ -1,22 +1,33 @@
 (function () {"use strict"; /*jslint browser:true regexp: true indent: 2 devel: true plusplus: true*/
+  function calculateAge(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday.getTime(),
+      ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
   var birthdayField = document.getElementById('dateofbirth'),
     birthday = null,
     patronCategory = document.getElementsByName('categorycode')[0],
     saveButtonWrapper = document.getElementsByClassName('action')[0],
-    saveButton = null,
+    saveButton = document.getElementsByName('save')[0],
     updateButton = document.createElement('input');
-  
-  if (patronCategory && patronCategory.value) {
-    if (birthdayField && birthdayField.value) {
-      birthday = new Date(birthdayField.value);
+
+  updateButton.type = "button";
+  updateButton.value = "Save & update patron type";
+  updateButton.style = "cursor:pointer; margin-right: 20px;";
+  updateButton.addEventListener('click', function () {
+    if (patronCategory.value === 'JU') {
+      patronCategory.value = 'AD';
+    } else if (patronCategory.value === 'LUJ') {
+      patronCategory.value = 'LU';
     }
-  
-    if (saveButtonWrapper) {
-      saveButton = saveButtonWrapper.children[0];
+    saveButton.click();
+  });
+
+  if (birthdayField && birthdayField.value && patronCategory && (patronCategory.value === 'LUJ' || patronCategory.value === 'JU')) {
+    birthday = new Date(birthdayField.value);
+    if (saveButtonWrapper && calculateAge(birthday) >= 16) {
+      saveButtonWrapper.insertBefore(updateButton, saveButtonWrapper.children[0]);
     }
-  
-    updateButton.type = "button";
-    updateButton.value = "Save & update patron type"
-    updateButton.style = 'cursor:pointer;';
   }
-}()); //end use strict
+  }()); //end use strict
