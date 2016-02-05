6e4daaddd4218d771132e5eb2288eef5cd1d67bd
Lucas Schneider <lrschneider94@gmail.com>
2016-02-04 23:36:28 -0600

    16.4.2 PROGRESS - Secondary sort by last name

--- data/kohaMods/sortItemCheckoutHistory.js
+++ data/kohaMods/sortItemCheckoutHistory.js
@@ -3,13 +3,15 @@
 
   function ItemHistoryEntry(htmlTR) {
     this.html = htmlTR;
+    this.name = "";
     this.date = "";
-    this.dateDue = "";
+    this.dueDate = "";
     this.returnDate = "";
 
     if (htmlTR.children.length > 7) {
       this.date = htmlTR.children[0].textContent.replace(/\s/g, "") !== "" ? new Date(htmlTR.children[0].textContent.replace(/\s/g, "")) : new Date();
-      this.dateDue = htmlTR.children[6].textContent.replace(/\s/g, "") !== "" ? new Date(htmlTR.children[6].textContent.replace(/\s/g, "")) : new Date();
+      this.name = htmlTR.children[1].textContent.trim();
+      this.dueDate = htmlTR.children[6].textContent.replace(/\s/g, "") !== "" ? new Date(htmlTR.children[6].textContent.replace(/\s/g, "")) : new Date();
       this.returnDate = htmlTR.children[7].textContent.replace(/\s/g, "") !== "" ? new Date(htmlTR.children[7].textContent.replace(/\s/g, "")) : new Date();
     }
   }
@@ -19,50 +21,74 @@
       // Due date, ASC
       case "dueASC":
         itemHistoryEntries.sort(function(a, b) {
-          return a.dateDue - b.dateDue;
+          if (a.dueDate < b.dueDate) return -1;
+          else if (b.dueDate > a.dueDate) return 1;
+          else if (a.name < b.name) return -1;
+          else if (a.name > b.name) return 1;
+          else return 0;
         });
         break;
       // Due date, DESC
       case "dueDESC":
         itemHistoryEntries.sort(function(a, b) {
-          return b.dateDue - a.dateDue;
+          if (b.dueDate < a.dueDate) return -1;
+          else if (b.dueDate > a.dueDate) return 1;
+          else if (a.name < b.name) return -1;
+          else if (a.name > b.name) return 1;
+          else return 0;
         });
         break;
       // Return date, ASC
       case "returnASC":
         itemHistoryEntries.sort(function(a, b) {
-          return a.returnDate - b.returnDate;
+          if (a.returnDate < b.returnDate) return -1;
+          else if (b.returnDate > a.returnDate) return 1;
+          else if (a.name < b.name) return -1;
+          else if (a.name > b.name) return 1;
+          else return 0;
         });
         break;
       // Return date, DESC
       case "returnDESC":
         itemHistoryEntries.sort(function(a, b) {
-          return b.returnDate - a.returnDate;
+          if (b.returnDate < a.returnDate) return -1;
+          else if (b.returnDate > a.returnDate) return 1;
+          else if (a.name < b.name) return -1;
+          else if (a.name > b.name) return 1;
+          else return 0;
         });
         break;
       // Checkout date, ASC
       case "checkoutASC":
         itemHistoryEntries.sort(function(a, b) {
-          return a.date - b.date;
+          if (a.date < b.date) return -1;
+          else if (b.date > a.date) return 1;
+          else if (a.name < b.name) return -1;
+          else if (a.name > b.name) return 1;
+          else return 0;
         });
         break;
       // Checkout date, DESC
       case "checkoutDESC":
       default:
         itemHistoryEntries.sort(function(a, b) {
-          return b.date - a.date;
+          if (b.date < a.date) return -1;
+          else if (b.date > a.date) return 1;
+          else if (a.name < b.name) return -1;
+          else if (a.name > b.name) return 1;
+          else return 0;
         });
     }
-    
+
     var tbody = document.createElement('tbody');
-    
+
     for (var trObj of itemHistoryEntries) {
       tbody.appendChild(trObj.html);
     }
-    
+
     var historyTable = document.getElementById('checkouthistt'),
     historyTbody;
-    
+
     if (historyTable) {
       historyTbody = historyTable.children[1];
       if (historyTbody) {
@@ -78,9 +104,9 @@
     itemRowArray,
     itemHistoryEntries = [],
     wrapper;
-    
+
   if (historyTable && h1Elts) {
-    
+
     itemRowArray = historyTable.children[1].children;
     if (itemRowArray && itemRowArray.length > 1) {
       for (var tr of itemRowArray) {
@@ -88,23 +114,23 @@
       }
       sortTable(itemHistoryEntries,"checkoutDESC");
     }
-    
+
     var sheet = window.document.styleSheets[1];
     sheet.insertRule('#sortSelectTable thead { font-weight: bold; }', sheet.cssRules.length);
     sheet.insertRule('#sortSelectTable tbody { cursor: pointer; }', sheet.cssRules.length);
     sheet.insertRule('#sortSelectTable td { text-align: center; }', sheet.cssRules.length);
     sheet.insertRule('.selectedSort { background: #99B7EE; cursor: default; }', sheet.cssRules.length);
-    
+
     wrapper = document.createElement('div');
     wrapper.id = "sortWrapper";
     wrapper.style = "padding-left:50px;";
     wrapper.innerHTML = '<h3>Sort by...</h3><table id="sortSelectTable"><thead><tr><td colspan="2">Checkout Date</td><td colspan="2">Due Date</td><td colspan="2">Return Date</td></tr></thead><tbody><tr id="sortOptions"><td id="checkoutASC">ASC</td><td id="checkoutDESC" class="selectedSort">DESC</td><td id="dueASC">ASC</td><td id="dueDESC">DESC</td><td id="returnASC">ASC</td><td id="returnDESC">DESC</td></tr></tbody></table>';
-    
+
     h1Parent = h1Elts[h1Elts.length-1].parentElement;
     h1Sibling = h1Parent.children[1];
-    
+
     h1Parent.insertBefore(wrapper, h1Sibling);
-    
+
     for (var td of document.getElementById('sortOptions').children) {
       td.addEventListener('click', function () {
         if (this.className !== "selectedSort") {
