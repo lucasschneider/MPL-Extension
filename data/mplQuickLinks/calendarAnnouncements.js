e738381e52823acd282a759c5fef76105ee0c3af
Lucas Schneider <lrschneider@gmail.com>
2016-06-11 13:20:33 -0500

    v17.0.4: Generate calendar announcements
    Changes to be committed:
    	modified:   data/mplQuickLinks/calendarAnnouncements.js
    	new file:   version/3338f72b-be88-4c0b-98ec-d24eeef851b4@jetpack-17.0.4.xpi

--- data/mplQuickLinks/calendarAnnouncements.js
+++ data/mplQuickLinks/calendarAnnouncements.js
@@ -6,8 +6,14 @@ var dRange = document.getElementsByName('drSel')[11],
   dr2Day = document.getElementById('dr2Day'),
   dr2Year = document.getElementById('dr2Year'),
   startDate,
-  endDate;
+  endDate,
+  eventTypeIndex = [0,1,2,3,4,5,6,7,9,10,11,12,13,15,16,17,20,21,24,30,31,32,36,37,38],
+  reportdisplayColVals = [true,true,false,true,true,true,false,false,true,false,true,false,false,false,false,false,true,true,false,false,true,false,false,false,true,false,false];
+  
+// Check date range radio button
+document.getElementsByName('drSel')[10].checked = true;
 
+// Prompt user for date range
 do {
   startDate = prompt("Enter the BEGINNING of the report's date range in the format MM/DD/YYYY:");
 } while (!/(0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d/.test(startDate));
@@ -25,7 +31,7 @@ switch(startDate.split("/")[0]) {
 	case "07": dr1Month.value = "7"; break;
 	case "08": dr1Month.value = "8"; break;
 	case "09": dr1Month.value = "9"; break;
-	default: dr1Month = startDate.split("/")[0]; break;
+	default: dr1Month.value = startDate.split("/")[0]; break;
 }
 
 switch(startDate.split("/")[1]) {
@@ -38,10 +44,10 @@ switch(startDate.split("/")[1]) {
 	case "07": dr1Day.value = "7"; break;
 	case "08": dr1Day.value = "8"; break;
 	case "09": dr1Day.value = "9"; break;
-	default: dr1Day = startDate.split("/")[1]; break;
+	default: dr1Day.value = startDate.split("/")[1]; break;
 }
 
-dr1Year = startDate.split("/")[2];
+dr1Year.value = startDate.split("/")[2];
 
 switch(endDate.split("/")[0]) {
 	case "01": dr2Month.value = "1"; break;
@@ -53,7 +59,7 @@ switch(endDate.split("/")[0]) {
 	case "07": dr2Month.value = "7"; break;
 	case "08": dr2Month.value = "8"; break;
 	case "09": dr2Month.value = "9"; break;
-	default: dr2Month = endDate.split("/")[0]; break;
+	default: dr2Month.value = endDate.split("/")[0]; break;
 }
 
 switch(endDate.split("/")[1]) {
@@ -66,7 +72,40 @@ switch(endDate.split("/")[1]) {
 	case "07": dr2Day.value = "7"; break;
 	case "08": dr2Day.value = "8"; break;
 	case "09": dr2Day.value = "9"; break;
-	default: dr2Day = endDate.split("/")[1]; break;
+	default: dr2Day.value = endDate.split("/")[1]; break;
 }
 
-dr2Year = endDate.split("/")[2];
+dr2Year.value = endDate.split("/")[2];
+
+// Include both ongoing and standard events
+document.getElementsByName('OngoingSel')[1].checked = true;
+
+// Check all age groups
+for (var i = 0; i < 7; i++) {
+	document.getElementsByName('AgeGroup')[i].checked = true;
+}
+
+// Check all libraries
+for (var i = 0; i < 9; i++) {
+	document.getElementsByName('Branch')[i].checked = true;
+}
+
+// Check relevant event types
+for  (var i = 0; i < eventTypeIndex.length; i++) {
+	document.getElementsByName('EventType')[eventTypeIndex[i]].checked = true;
+}
+
+// Set display format to table
+document.getElementsByName('df')[1].click();
+
+
+// Set output format to table
+document.getElementsByName('dt')[2].checked = true;
+
+// Combine recurring events
+document.getElementsByName('collapseRecurr')[0].click();
+
+// Check revelant report display columns
+for (var i = 0; i < reportdisplayColVals.length; i++) {
+	document.getElementsByName('DisplayCol')[i].checked = reportdisplayColVals[i];
+}
